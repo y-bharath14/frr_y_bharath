FRRouting/frr

isisd: fix boolean display is show isis database detail json
Display booleans values with string such as "", "*", "yes" and "no" are
not correct on JSON.

Display boolean values as true or false.

Fixes: a2cac12a63 ("isisd: Add json to show isis database command.")
Signed-off-by: Louis Scalbert <louis.scalbert@6wind.com>

isisd/isis_lsp.c
@@ -754,7 +754,7 @@ void lsp_print_json(struct isis_lsp *lsp, struct json_object *json,
 
 	lspid_print(lsp->hdr.lsp_id, LSPid, sizeof(LSPid), dynhost, 1, isis);
 	json_object_string_add(json, "id", LSPid);
-	json_object_boolean_add(json, "own", lsp->own_lsp ? "*" : " ");
+	json_object_boolean_add(json, "own", lsp->own_lsp ? true : false);
 	json_object_int_add(json, "pdu-len", lsp->hdr.pdu_len);
 	snprintfrr(buf, sizeof(buf), "0x%08x", lsp->hdr.seqno);
 	json_object_string_add(json, "seq-number", buf);

y-bharath14/frr_y_bharath

isisd/isis_lsp.c
@@ -773,7 +773,7 @@ void lsp_print_json(struct isis_lsp *lsp, struct json_object *json,
 	own_json = json_object_new_object();
 	json_object_object_add(json, "lsp", own_json);
 	json_object_string_add(own_json, "id", LSPid);
-	json_object_string_add(own_json, "own", lsp->own_lsp ? "*" : " ");
+	json_object_boolean_add(own_json, "own", lsp->own_lsp ? true : false);
 	json_object_int_add(json, "pdu-len", lsp->hdr.pdu_len);
 	snprintfrr(buf, sizeof(buf), "0x%08x", lsp->hdr.seqno);
 	json_object_string_add(json, "seq-number", buf);