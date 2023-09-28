FRRouting/frr

commit 34ea39b65a2d6dd48df15d9d8a929c1a6f128122

bgpd: Check NHT change for triggering EVPN import or unimport
Ensure that only if there is a change to the path's validity based
on the NHT update, EVPN import or unimport is invoked.

Signed-off-by: Vivek Venkatraman <vivek@cumulusnetworks.com>

bgpd/bgp_nht.c
@@ -736,7 +736,8 @@ static void evaluate_paths(struct bgp_nexthop_cache *bnc)
 		 * nexthops with labels
 		 */
 
-		int bnc_is_valid_nexthop = 0;
+		bool bnc_is_valid_nexthop = false;
+		bool path_valid = false;
 
 		if (safi == SAFI_UNICAST &&
 			path->sub_type == BGP_ROUTE_IMPORTED &&
@@ -744,7 +745,7 @@ static void evaluate_paths(struct bgp_nexthop_cache *bnc)
 			path->extra->num_labels) {
 
 			bnc_is_valid_nexthop =
-				bgp_isvalid_labeled_nexthop(bnc) ? 1 : 0;
+				bgp_isvalid_labeled_nexthop(bnc) ? true : false;
 		} else {
 			if (bgp_update_martian_nexthop(
 				    bnc->bgp, afi, safi, path->type,
@@ -755,7 +756,7 @@ static void evaluate_paths(struct bgp_nexthop_cache *bnc)
 						__func__, rn, bgp_path->name);
 			} else
 				bnc_is_valid_nexthop =
-					bgp_isvalid_nexthop(bnc) ? 1 : 0;
+					bgp_isvalid_nexthop(bnc) ? true : false;
 		}
 
 		if (BGP_DEBUG(nht, NHT)) {
@@ -774,20 +775,6 @@ static void evaluate_paths(struct bgp_nexthop_cache *bnc)
 					afi, safi, rn, bgp_path->name_pretty,
 					path->flags);
 		}
-		if ((CHECK_FLAG(path->flags, BGP_PATH_VALID) ? 1 : 0)
-		    != bnc_is_valid_nexthop) {
-			if (CHECK_FLAG(path->flags, BGP_PATH_VALID)) {
-				bgp_aggregate_decrement(bgp_path, p, path, afi,
-							safi);
-				bgp_path_info_unset_flag(rn, path,
-							 BGP_PATH_VALID);
-			} else {
-				bgp_path_info_set_flag(rn, path,
-						       BGP_PATH_VALID);
-				bgp_aggregate_increment(bgp_path, p, path, afi,
-							safi);
-			}
-		}
 
 		/* Copy the metric to the path. Will be used for bestpath
 		 * computation */
@@ -801,13 +788,33 @@ static void evaluate_paths(struct bgp_nexthop_cache *bnc)
 		    || CHECK_FLAG(bnc->change_flags, BGP_NEXTHOP_CHANGED))
 			SET_FLAG(path->flags, BGP_PATH_IGP_CHANGED);
 
-		if (safi == SAFI_EVPN && bgp_evpn_is_prefix_nht_supported(p)) {
-			if (CHECK_FLAG(path->flags, BGP_PATH_VALID))
-				bgp_evpn_import_route(bgp_path, afi, safi, p,
-						      path);
-			else
-				bgp_evpn_unimport_route(bgp_path, afi, safi, p,
-							path);
+		path_valid = !!CHECK_FLAG(path->flags, BGP_PATH_VALID);
+		if (path_valid != bnc_is_valid_nexthop) {
+			if (path_valid) {
+				/* No longer valid, clear flag; also for EVPN
+				 * routes, unimport from VRFs if needed.
+				 */
+				bgp_aggregate_decrement(bgp_path, p, path, afi,
+							safi);
+				bgp_path_info_unset_flag(rn, path,
+							 BGP_PATH_VALID);
+				if (safi == SAFI_EVPN &&
+				    bgp_evpn_is_prefix_nht_supported(&rn->p))
+					bgp_evpn_unimport_route(bgp_path,
+						afi, safi, &rn->p, path);
+			} else {
+				/* Path becomes valid, set flag; also for EVPN
+				 * routes, import from VRFs if needed.
+				 */
+				bgp_path_info_set_flag(rn, path,
+						       BGP_PATH_VALID);
+				bgp_aggregate_increment(bgp_path, p, path, afi,
+							safi);
+				if (safi == SAFI_EVPN &&
+				    bgp_evpn_is_prefix_nht_supported(&rn->p))
+					bgp_evpn_import_route(bgp_path,
+						afi, safi, &rn->p, path);
+			}
 		}
 
 		bgp_process(bgp_path, rn, afi, safi);

y-bharath14/frr_y_bharath

commit 08e1be31e88422cc1fa6c9ecca99e7e3b92e71e4

bgpd: fix import of evpn routes into vni/vrf/esi after nexthop update
Related to issue #10271. When importing prefixes from EVPN, any changes
to the BGP nexthop associated with those prefixes, such as a change in
the route metric to the peer, will not trigger a re-evaluation of the
imported prefixes.

In evaluate_path(), the import is only triggered when the validity of
the path changes, which is not the case when, for example, the route
metric to the peer changes.

Request import or unimport when BGP_PATH_IGP_CHANGED is set but the path
validity is unchanged. In install_evpn_route_entry_in_vrf(), do not skip
VRF import when BGP_PATH_IGP_CHANGED is set.

Fixes: 34ea39b65a ("bgpd: Check NHT change for triggering EVPN import or unimport")
Signed-off-by: Louis Scalbert <louis.scalbert@6wind.com>

bgpd/bgp_evpn.c
@@ -2968,8 +2968,9 @@ static int install_evpn_route_entry_in_vrf(struct bgp *bgp_vrf,
 		pi = bgp_create_evpn_bgp_path_info(parent_pi, dest, &attr);
 		new_pi = true;
 	} else {
-		if (attrhash_cmp(pi->attr, &attr)
-		    && !CHECK_FLAG(pi->flags, BGP_PATH_REMOVED)) {
+		if (attrhash_cmp(pi->attr, &attr) &&
+		    !CHECK_FLAG(parent_pi->flags, BGP_PATH_IGP_CHANGED) &&
+		    !CHECK_FLAG(pi->flags, BGP_PATH_REMOVED)) {
 			bgp_dest_unlock_node(dest);
 			return 0;
 		}

bgpd/bgp_nht.c
@@ -1390,6 +1390,21 @@ void evaluate_paths(struct bgp_nexthop_cache *bnc)
 								   bgp_path,
 								   path);
 			}
+		} else if (safi == SAFI_EVPN &&
+			   CHECK_FLAG(path->flags, BGP_PATH_IGP_CHANGED) &&
+			   bgp_evpn_is_prefix_nht_supported(
+				   bgp_dest_get_prefix(dest))) {
+			if (bnc_is_valid_nexthop)
+				/* import the EVPN routes if the path validity
+				 * changed or update existing routes.
+				 */
+				bgp_evpn_import_route(bgp_path, afi, safi,
+						      bgp_dest_get_prefix(dest),
+						      path);
+			else
+				bgp_evpn_unimport_route(bgp_path, afi, safi,
+							bgp_dest_get_prefix(dest),
+							path);
 		}
 
 		bgp_process(bgp_path, dest, afi, safi);