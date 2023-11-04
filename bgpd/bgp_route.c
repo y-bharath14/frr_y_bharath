7d5056e1ea7241df6692eeca72039726763cb7c7
Donald Sharp <sharpd@nvidia.com>
2023-11-03 23:26:56 -0400

    bgpd: add some counters not displayed yet
    Add some counters to keep track how often stuff is done.
    This is mainly for us developers.
    
    Signed-off-by: Donald Sharp <sharpd@nvidia.com>

--- bgpd/bgp_route.c
+++ bgpd/bgp_route.c
@@ -654,6 +654,8 @@ int bgp_path_info_cmp(struct bgp *bgp, struct bgp_path_info *new,
 	struct bgp_path_info *bpi_ultimate;
 	struct peer *peer_new, *peer_exist;
 
+	bgp->bestpath_runs++;
+
 	*paths_eq = 0;
 
 	/* 0. Null check. */
@@ -3695,8 +3697,10 @@ void bgp_process(struct bgp *bgp, struct bgp_dest *dest, afi_t afi, safi_t safi)
 	int pqnode_reuse = 0;
 
 	/* already scheduled for processing? */
-	if (CHECK_FLAG(dest->flags, BGP_NODE_PROCESS_SCHEDULED))
+	if (CHECK_FLAG(dest->flags, BGP_NODE_PROCESS_SCHEDULED)) {
+		bgp->node_already_on_queue++;
 		return;
+	}
 
 	/* If the flag BGP_NODE_SELECT_DEFER is set, do not add route to
 	 * the workqueue
@@ -3705,6 +3709,7 @@ void bgp_process(struct bgp *bgp, struct bgp_dest *dest, afi_t afi, safi_t safi)
 		if (BGP_DEBUG(update, UPDATE_OUT))
 			zlog_debug("BGP_NODE_SELECT_DEFER set for route %p",
 				   dest);
+		bgp->node_deferred_on_queue++;
 		return;
 	}
 
@@ -12594,6 +12599,12 @@ DEFUN(show_ip_bgp_afi_safi_statistics, show_ip_bgp_afi_safi_statistics_cmd,
 		json = json_object_new_object();
 		json_object_object_add(json, get_afi_safi_str(afi, safi, true),
 				       json_afi_safi);
+		json_object_int_add(json, "bgpBestPathCalls",
+				    bgp->bestpath_runs);
+		json_object_int_add(json, "bgpNodeOnQueue",
+				    bgp->node_already_on_queue);
+		json_object_int_add(json, "bgpNodeDeferredOnQueue",
+				    bgp->node_deferred_on_queue);
 		vty_json(vty, json);
 	}
 	return ret;
