f282af8ed5b87808ab47af0686551cc76305adf2
Philippe Guibert <philippe.guibert@6wind.com>
2024-01-05 16:55:24 +0100

    bgpd: add nhg group support for multiple path routes
    The nexthop group support is not applicable to ECMP BGP paths.
    For instance, when BGP addpath is configured, only prefixes
    associated to a nexthop are using nexthop groups at BGP level.
    
    Unlock the possibility to use nexthop groups for ECMP paths.
    The choice is done to create a nexthop group hierarchy with
    multiple nexthops.
    
    For each path to install, two nexthop groups will be used:
    nexthops and groups.
    - Link the parent nexthop group to the path (with pi->bgp_nhg)
    - Link the original bgp_nhg_cache nexthop to the path (with pi->bgp_nhg_nexthop).
    - Handle the installation order at proto level: the nexthop first, the parent after
    - Add nexthop extension in 'show bgp nexthop-group'
    - unlock the ability to handle multiple paths with NHGs
    - A double hash list is used to handle frr_each_safe() limitations:
    this technical choice is done in order to be able to detach both
    nexthops and groups in a single walk. Otherwise, asan errors are
    detected.
    
    Signed-off-by: Philippe Guibert <philippe.guibert@6wind.com>

--- bgpd/bgp_route.c
+++ bgpd/bgp_route.c
@@ -3479,7 +3479,7 @@ static void bgp_process_main_one(struct bgp *bgp, struct bgp_dest *dest,
 	     (pi != NULL) && (nextpi = pi->next, 1); pi = nextpi) {
 		if (!CHECK_FLAG(pi->flags, BGP_PATH_SELECTED) &&
 		    !CHECK_FLAG(pi->flags, BGP_PATH_MULTIPATH))
-			bgp_nhg_path_unlink(pi);
+			bgp_nhg_path_nexthop_unlink(pi, false);
 	}
 
 	/* call bmp hook for loc-rib route update / withdraw after flags were
@@ -3952,6 +3952,8 @@ void bgp_rib_remove(struct bgp_dest *dest, struct bgp_path_info *pi,
 			}
 		}
 	}
+
+	bgp_nhg_path_nexthop_unlink(pi, false);
 
 	hook_call(bgp_process, peer->bgp, afi, safi, dest, peer, true);
 	bgp_process(peer->bgp, dest, afi, safi);
@@ -5659,6 +5661,8 @@ static void bgp_clear_node_complete(struct work_queue *wq)
 
 	/* Tickle FSM to start moving again */
 	BGP_EVENT_ADD(peer->connection, Clearing_Completed);
+	if (bgp_option_check(BGP_OPT_NHG))
+		bgp_nhg_clear_nhg_nexthop();
 
 	peer_unlock(peer); /* bgp_clear_route */
 }
@@ -5683,7 +5687,7 @@ static void bgp_clear_node_queue_init(struct peer *peer)
 }
 
 static void bgp_clear_route_table(struct peer *peer, afi_t afi, safi_t safi,
-				  struct bgp_table *table)
+				  struct bgp_table *table, bool nexthop_unlink)
 {
 	struct bgp_dest *dest;
 	int force = peer->bgp->process_queue ? 0 : 1;
@@ -5752,6 +5756,9 @@ static void bgp_clear_route_table(struct peer *peer, afi_t afi, safi_t safi,
 			if (pi->peer != peer)
 				continue;
 
+			if (nexthop_unlink && !force)
+				bgp_nhg_path_nexthop_unlink(pi, false);
+
 			if (force) {
 				dest = bgp_path_info_reap(dest, pi);
 				assert(dest);
@@ -5773,7 +5780,8 @@ static void bgp_clear_route_table(struct peer *peer, afi_t afi, safi_t safi,
 	return;
 }
 
-void bgp_clear_route(struct peer *peer, afi_t afi, safi_t safi)
+void bgp_clear_route(struct peer *peer, afi_t afi, safi_t safi,
+		     bool nexthop_unlink)
 {
 	struct bgp_dest *dest;
 	struct bgp_table *table;
@@ -5802,7 +5810,7 @@ void bgp_clear_route(struct peer *peer, afi_t afi, safi_t safi)
 		peer_lock(peer);
 
 	if (safi != SAFI_MPLS_VPN && safi != SAFI_ENCAP && safi != SAFI_EVPN)
-		bgp_clear_route_table(peer, afi, safi, NULL);
+		bgp_clear_route_table(peer, afi, safi, NULL, nexthop_unlink);
 	else
 		for (dest = bgp_table_top(peer->bgp->rib[afi][safi]); dest;
 		     dest = bgp_route_next(dest)) {
@@ -5810,7 +5818,8 @@ void bgp_clear_route(struct peer *peer, afi_t afi, safi_t safi)
 			if (!table)
 				continue;
 
-			bgp_clear_route_table(peer, afi, safi, table);
+			bgp_clear_route_table(peer, afi, safi, table,
+					      nexthop_unlink);
 		}
 
 	/* unlock if no nodes got added to the clear-node-queue. */
@@ -5824,7 +5833,7 @@ void bgp_clear_route_all(struct peer *peer)
 	safi_t safi;
 
 	FOREACH_AFI_SAFI (afi, safi)
-		bgp_clear_route(peer, afi, safi);
+		bgp_clear_route(peer, afi, safi, true);
 
 #ifdef ENABLE_BGP_VNC
 	rfapiProcessPeerDown(peer);
@@ -5942,6 +5951,9 @@ void bgp_clear_stale_route(struct peer *peer, afi_t afi, safi_t safi)
 				break;
 			}
 	}
+	/* all routes marked as remove - let us clean nhg context */
+	if (bgp_option_check(BGP_OPT_NHG))
+		bgp_nhg_clear_nhg_nexthop();
 }
 
 void bgp_set_stale_route(struct peer *peer, afi_t afi, safi_t safi)
