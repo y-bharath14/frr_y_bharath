FRRouting/frr

isisd: Handle locator received from SID Manager
Extend IS-IS to process a GET_LOCATOR request coming from a daemon.
IS-IS processes the locator information received from the SRv6 SID
Manager and then interacts with the SID Manager to get/release a SID.

Signed-off-by: Carmine Scarpitta <cscarpit@cisco.com>

lib/srv6.c
@@ -138,6 +138,21 @@ struct srv6_locator_chunk *srv6_locator_chunk_alloc(void)
 	return chunk;
 }
 
+void srv6_locator_copy(struct srv6_locator *copy,
+		       const struct srv6_locator *locator)
+{
+	strlcpy(copy->name, locator->name, sizeof(locator->name));
+	copy->prefix = locator->prefix;
+	copy->block_bits_length = locator->block_bits_length;
+	copy->node_bits_length = locator->node_bits_length;
+	copy->function_bits_length = locator->function_bits_length;
+	copy->argument_bits_length = locator->argument_bits_length;
+	copy->algonum = locator->algonum;
+	copy->current = locator->current;
+	copy->status_up = locator->status_up;
+	copy->flags = locator->flags;
+}
+
 void srv6_locator_free(struct srv6_locator *locator)
 {
 	if (locator) {

y-bharath14/frr_y_bharath

lib/srv6.c
@@ -137,6 +137,21 @@ struct srv6_locator_chunk *srv6_locator_chunk_alloc(void)
 	return chunk;
 }
 
+void srv6_locator_copy(struct srv6_locator *copy,
+		       const struct srv6_locator *locator)
+{
+	strncpy(copy->name, locator->name, sizeof(locator->name));
+	copy->prefix = locator->prefix;
+	copy->block_bits_length = locator->block_bits_length;
+	copy->node_bits_length = locator->node_bits_length;
+	copy->function_bits_length = locator->function_bits_length;
+	copy->argument_bits_length = locator->argument_bits_length;
+	copy->algonum = locator->algonum;
+	copy->current = locator->current;
+	copy->status_up = locator->status_up;
+	copy->flags = locator->flags;
+}
+
 void srv6_locator_free(struct srv6_locator *locator)
 {
 	if (locator) {